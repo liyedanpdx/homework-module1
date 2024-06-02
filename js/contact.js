bf0577d6240d7368a83298f0fe60dfbbdc1b0c42
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-06-02 01:41:10 -0500

    email/phone show 2

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,24 @@
 var contactSelect=document.getElementById('contactSelect');
-contactSelect.addEventListener("onchange", contactInputShow);
+contactSelect.addEventListener("change", contactInputShow);
 
 function disableEmailShow(noShow){
     if (noShow==true){
-        document.getElementById('enterEmailBlock').style.display=none;
+        document.getElementById('enterEmailBlock').style.display="none";
     } else {
-        document.getElementById('enterEmailBlock').style.display=block;
+        document.getElementById('enterEmailBlock').style.display="block";
     }
 }
 function disablePhoneShow(noShow){
     if (noShow==true){
-        document.getElementById('enterPhoneBlock').style.display=none;
+        document.getElementById('enterPhoneBlock').style.display="none";
     } else {
-        document.getElementById('enterPhoneBlock').style.display=block;
+        document.getElementById('enterPhoneBlock').style.display="block";
     }
 }
 
 function contactInputShow(){
     var contactSelectValue=document.getElementById('contactSelect').value;
+    console.log("contactSelectValue: " + contactSelectValue);
     if (contactSelectValue=='Email'){
         disableEmailShow(false);
         disablePhoneShow(true);

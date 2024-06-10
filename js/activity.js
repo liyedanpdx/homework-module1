332e22101dc412742185972f82d90df7d9706e8b
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-06-09 20:32:38 -0500

    click color block2

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-    $("tbody tr td").click(function(){
-        console.log('click');
-        if($(this).css("background-color") === "rgb(238, 255, 230)"){
-            $(this).css("background-color", "green");
-        } else {
-            $(this).css("background-color", "#EEFFE6");
+    $("tbody tr td:not(:first-child)").each(function(){
+        if($(this).text() !== 'Not Available'){
+            $(this).css('cursor', 'pointer');
+        }
+    });
+    $("tbody tr td:not(:first-child)").click(function(){
+        console.log($(this).css("background-color"));
+        if($(this).text() !== 'Not Available'){
+            if($(this).css("background-color") !== "rgb(157, 208, 51)"){
+                $(this).css({
+                    "background-color": "rgb(157,208,51)",
+                    "color": "white",
+                    "font-weight": "bold"
+                });
+            } else {
+                $(this).css({
+                    "background-color": "#EEFFE6",
+                    "color": "black",
+                    "font-weight": "normal"
+                });
+            }
         }
     });
 });
